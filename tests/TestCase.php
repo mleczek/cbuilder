02bd5f69b8a9c329f2a838aeac0f39d19c269d85
Wojciech Mleczek <mleczek.wojtek@gmail.com>
2017-02-10 19:17:48 +0100

    Test package file "name" key

---
+++ tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Mleczek\CBuilder\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+
+class TestCase extends BaseTestCase
+{
+    const PROJECT_DIR = __DIR__ .'/..';
+}
