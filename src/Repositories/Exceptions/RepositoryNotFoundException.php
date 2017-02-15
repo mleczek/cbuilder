a6cbc4e5c19a8d8e4ddc84b685bdca80b0a60c21
Wojciech Mleczek <mleczek.wojtek@gmail.com>
2017-02-15 10:22:03 +0100

    Repositories container

---
+++ src/Repositories/Exceptions/RepositoryNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Mleczek\CBuilder\Repositories\Exceptions;
+
+
+class RepositoryNotFoundException extends \Exception
+{
+
+}
--- src/Repositories/Providers/LocalRepository.php
+++ src/Repositories/Providers/LocalRepository.php
@@ -5,6 +5,7 @@ namespace Mleczek\CBuilder\Repositories\Providers;
 use Mleczek\CBuilder\Downloaders\Downloader;
 use Mleczek\CBuilder\Downloaders\Providers\SymlinkDownloader;
 use Mleczek\CBuilder\Repositories\Repository;
+use Mleczek\CBuilder\System\Environment;
 use Mleczek\CBuilder\Versions\Providers\ConstVersion;
 
 class LocalRepository implements Repository
@@ -25,15 +26,22 @@ class LocalRepository implements Repository
     private $downloader;
 
     /**
+     * @var Environment
+     */
+    private $env;
+
+    /**
      * @param SymlinkDownloader $downloader
      * @param ConstVersion $versions
+     * @param Environment $env
      * @param string $dir
      */
-    public function __construct(SymlinkDownloader $downloader, ConstVersion $versions, $dir)
+    public function __construct(SymlinkDownloader $downloader, ConstVersion $versions, Environment $env, $dir)
     {
         $this->dir = $dir;
         $this->versions = $versions;
         $this->downloader = $downloader;
+        $this->env = $env;
     }
 
     /**
@@ -58,7 +66,7 @@ class LocalRepository implements Repository
     public function download($package, $constraint)
     {
         $src = $this->dir . '/' . $package;
-        $dest = 'cmodules/' . $package; // FIXME: Read modules dir from config
+        $dest = $this->env->config('modules.dir') . '/' . $package;
 
         return $this->downloader
             ->from($src)->to($dest)
